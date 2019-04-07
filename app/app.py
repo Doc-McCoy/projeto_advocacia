8e3400692717e954818f6cb8706a1975e58e785d
Renan Santana <renan_ota@hotmail.com>
2019-04-07 14:21:17 -0300

    Inserção de usuário, tratamento de mensagens flash

--- app/app.py
+++ app/app.py
@@ -73,8 +73,21 @@ def clientes():
 def usuarios():
     if request.method == 'GET':
         return render_template('usuarios.html', title="Usuários")
+
     if request.method == 'POST':
-        return "<h1> Salvou! </h1>"
+
+        usuario = Usuarios(
+            usu_nome = request.form['usu_nome'],
+            usu_email = request.form['usu_email'],
+            usu_senha = request.form['usu_senha'],
+            usu_departamento = request.form['usu_departamento'],
+        )
+
+        db.session.add(usuario)
+        db.session.commit()
+
+        flash("Usuário cadastrado com sucesso.", "success")
+        return redirect(url_for('usuarios'))
 
 @app.route('/logout')
 @login_required
